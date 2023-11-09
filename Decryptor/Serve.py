3227a3d1138ceb4be59f8758f29047327cb357a1
incendiary <a.horsewood@googlemail.com>
2023-11-09 13:51:26 +0800

    workign from config file and vars now

--- Decryptor/Serve.py
+++ Decryptor/Serve.py
@@ -1,13 +1,40 @@
-from flask import Flask, send_file, render_template, make_response
+import uuid
+from flask import Flask, send_file, abort, render_template
+import configparser
+import os
+from werkzeug.utils import secure_filename
+import hashlib
+import base64
+import re
 
 app = Flask(__name__)
 
+# Read the configuration
+config = configparser.ConfigParser()
+config.read('../config.ini')
+server_hostname = config['DEFAULT']['ServerHostname']
+num_files = int(config['DEFAULT']['NumberOfFiles'])
+key = config['DEFAULT']['EncryptionKey']
+download_name = config['DEFAULT']['DownloadName']
+downloads_path = 'Downloads/parts'  # Assuming the parts folder is in the Downloads directory
+
+# Generate a unique FileGUID when the server starts
+file_guid = str(uuid.uuid4())
+
+encoded_key = base64.b64encode(key.encode('utf-8')).decode('utf-8')
+
+def compute_hash(file_path):
+    hash_sha256 = hashlib.sha256()
+    with open(file_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_sha256.update(chunk)
+    return hash_sha256.hexdigest()
+    
+
 @app.after_request
 def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
+    # Add headers to both force latest IE rendering engine or Chrome Frame,
+    # and also to cache the rendered page for 10 minutes.
     response.headers["Cache-Control"] = "no-store"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "0"
@@ -15,32 +42,55 @@ def add_header(response):
 
 @app.route('/')
 def index():
-    # Serve your 'SideLoadMe.html' as the default page
-    return render_template('SideLoadMe.html')
+    # Scan the directory and create a dictionary with file names and their hashes
+    file_parts = {part: compute_hash(os.path.join(downloads_path, part)) for part in os.listdir(downloads_path)}
+    sorted_file_parts = dict(sorted(file_parts.items(), key=lambda item: int(re.search(r'part(\d+)', item[0]).group(1))))
 
-@app.route('/iv')
-def get_iv():
-    # Serve the 'iv.bin' file
-    response = make_response(send_file('iv.bin', mimetype='application/octet-stream'))
-    return response
+    return render_template('sideloadme.html', server_hostname=server_hostname, file_parts=sorted_file_parts,
+                           file_guid=file_guid, encoded_key=encoded_key, download_name=download_name)
 
-@app.route('/salt')
-def get_salt():
-    # Serve the 'salt.bin' file
-    response = make_response(send_file('salt.bin', mimetype='application/octet-stream'))
-    return response
+@app.route('/<uid>/<path:filename>')
+def serve_file_part(uid, filename):
+    if uid != file_guid:
+        abort(404)
+    # Secure the filename to avoid any directory traversal attack
+    secure_filename_path = secure_filename(filename)
+    # Build the complete file path
+    file_path = os.path.join(downloads_path, secure_filename_path)
 
-@app.route('/file_part0')
-def get_part0():
-    # Serve the 'file_part0' file
-    response = make_response(send_file('file_part0', mimetype='application/octet-stream'))
-    return response
+    print(file_path)
 
-@app.route('/file_part1')
-def get_part1():
-    # Serve the 'file_part1' file
-    response = make_response(send_file('file_part1', mimetype='application/octet-stream'))
-    return response
+    # Check if file exists
+    if os.path.isfile(file_path):
+        return send_file(file_path, mimetype='application/octet-stream')
+    else:
+        abort(404)  # If the file doesn't exist, return a 404 not found error.
+
+# Serve the IV and Salt files, with added directory traversal protection
+@app.route('/<uid>/iv')
+def get_iv(uid):
+    if uid != file_guid:
+        abort(404)
+    # Assuming the iv.bin is directly under the Downloads directory
+    iv_path = os.path.join('Downloads', 'iv.bin')
+    return send_file(iv_path, mimetype='application/octet-stream')
+
+@app.route('/<uid>/salt')
+def get_salt(uid):
+    if uid != file_guid:
+        abort(404)
+    # Assuming the salt.bin is directly under the Downloads directory
+    salt_path = os.path.join('Downloads', 'salt.bin')
+    return send_file(salt_path, mimetype='application/octet-stream')
+
+# Print all the routes for the Flask app
+def print_routes(app):
+    print("Registered routes:")
+    for rule in app.url_map.iter_rules():
+        print(f"{rule.endpoint}: {rule}")
 
 if __name__ == '__main__':
-    app.run(port=80)
+    print("GID Generated is: " + file_guid)
+    print("Key Info - Value: %s, Encoded Value:%s" % (key, encoded_key))
+    print_routes(app)  # This will print all routes
+    app.run(port=int(config['SERVER']['Port']), debug=config['SERVER'].getboolean('DebugMode'))
--- Encryptors/python/Slice.py
+++ Encryptors/python/Slice.py
@@ -2,19 +2,24 @@ from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from Crypto.Util.Padding import pad
 from Crypto.Protocol.KDF import PBKDF2
+import configparser
 import sys
 import os
 
+# Read the configuration
+config = configparser.ConfigParser()
+config.read('../../config.ini')
+num_files = int(config['DEFAULT']['NumberOfFiles'])
+password = config['DEFAULT']['EncryptionKey'].encode()  #
+
 # Arguments from command line
-num_files = int(sys.argv[1])
-file_path = sys.argv[2]
+file_path = sys.argv[1]
 
 # Read binary file
 with open(file_path, 'rb') as file:
     data = file.read()
 
 # Key derivation
-password = b'exampleKey'
 salt = os.urandom(16)  # Should be securely generated and consistent for decryption
 key = PBKDF2(password, salt, dkLen=16, count=100000, hmac_hash_module=SHA256)
 
@@ -24,11 +29,11 @@ cipher = AES.new(key, AES.MODE_CBC, iv)
 ciphertext = cipher.encrypt(pad(data, AES.block_size))
 
 # Save IV to file
-with open('iv.bin', 'wb') as iv_file:
+with open('../../Decryptor/Downloads/iv.bin', 'wb') as iv_file:
     iv_file.write(iv)
 
 # Save salt to file
-with open('salt.bin', 'wb') as salt_file:
+with open('../../Decryptor/Downloads/salt.bin', 'wb') as salt_file:
     salt_file.write(salt)
 
 # Write encrypted file without the IV
@@ -51,7 +56,10 @@ for i in range(num_files):
         chunk_data = enc_file.read(part_size)
         # Write the chunk to a new part file
         with open(f'{file_path}_part{i}', 'wb') as part_file:
+            print(f"Saving " + file_path)
             part_file.write(chunk_data)
 
 print(f"Encrypted file split into {num_files} parts.")
 print(f"IV saved to iv.bin.")
+print(f"Key is %s " % password)
+print(f"salt saved to salt.bin.")
